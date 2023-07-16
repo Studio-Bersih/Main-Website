9dad9d4c62666138771a2ac38a98e84936ca7dee
okuruu <Phantom_Ghost@outlook.co.id>
2023-07-16 14:25:33 +0700

    Little Header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
-<div class="alert alert-info">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-    <span>New software update available.</span>
+<div class="container mx-auto">
+    <div class="d-flex content-center">
+        <div class="text-center my-5">
+            <h1>Studio Bersih</h1>
+            <h3>Lakukan lebih baik!</h3>
+        </div>
+        <div class="my-5">
+            <button type="button" class="btn btn-accent">Masuk</button>
+            <button type="button" class="btn btn-secondary">Semua Sistem</button>
+        </div>
+     </div>
 </div>
